60fcad884e0f75dc2a7acd14b14f5dac3dc145a7
winigajordan <winigajordan@gmail.com>
2022-08-02 02:45:08 +0000

    debut apprentissage django

---
+++ code1/src/DocBlog/views.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index (request):
+    #return HttpResponse('<h1>bonjour index</h1>')
+    date = datetime.today()
+    return render(
+        request,
+        "index.html",
+        context={
+            "nom":"rema",
+            "prenom": "jordan",
+            "date": date
+        })
+
+def vue_de_test(request):
+    return HttpResponse('<h1>bonjour</h1>')
